295858062879ffbc22c233526c111e1e3583a62d
Luka Potočnik <luka.potochnik@gmail.com>
2022-11-17 09:23:12 +0100

    Remove ManageIQ domain

--- ManageIQ/Infrastructure/Configured_System/Provisioning/Email.class/__methods__/request_approved.rb
+++
@@ -1,4 +0,0 @@
-#
-# Description: Place holder for Provision Request Approved email #
-#
-$evm.log("warn", "[DEPRECATION] This method will be deprecated. Please use similarly named method from System/Notification/Email class.")
