e8b586eba8c51075752d584eeb689356d94940e5
Luka Potočnik <luka.potochnik@gmail.com>
2022-11-17 09:23:12 +0100

    Remove ManageIQ domain

--- ManageIQ/Service/Retirement/StateMachines/Methods.class/__methods__/retire_service.rb
+++
@@ -1,5 +0,0 @@
-#
-# Description: Placeholder method for custom automation
-#
-
-# service = $evm.root["service"]
--- ManageIQ/System/Policy.class/__methods__/vmcloudreconfigure_request_auto_approve.rb
+++
@@ -1,5 +0,0 @@
-#
-# Description: This method auto-approves the VM Cloud Reconfiguration request
-#
-$evm.log("info", "AUTO-APPROVING")
-$evm.root["miq_request"].approve("admin", "VM Cloud Reconfiguration Auto-Approved")
